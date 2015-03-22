d1d20fddbaf06bd81f5578b026e9c3939ab2748b
JJ <jjeg1979@gmail.com>
2015-03-22 12:42:29 +0100

    second commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include "std_lib_facilities.h"
+
+int main()    // C++ programs start by executing the function main
+{
+     cout << "Hello GitWorld!\n";     // output "Hello GitWorld!\n";
+     keep_window_open();              // wait for a character to be entered
+     return 0;
+}
